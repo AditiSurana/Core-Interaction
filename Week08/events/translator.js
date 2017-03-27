f73ab62c0260c70c0afb64f9afb39dec0d94b0ae
Sam Galison <samface@gmail.com>
2017-03-27 15:16:55 -0400

    added hints + notes

--- Week08/events/translator.js
+++ Week08/events/translator.js
@@ -1,30 +1,50 @@
+var alphabet = "abcdefghijklmnopqrstuvwxyz"; // a string full of letters
+var letter_array = []; // make an empty array to store the letters
+
+for(i = 0; i < alphabet.length; i++) {
+	// for every letter in the string...
+	letter_array.push(alphabet[i]); // ...add that letter to the array
+}
+
+
+
+// once that array's set up, you can use the cleaner "forEach" syntax to iterate through the array, calling a function with each element as an argument:
+letter_array.forEach(make_a_div);
+
+
+// then you have to define that "make_a_div" function:
+function make_a_div(element, index) {
+	// element = the individual list item (gets passed in automatically by forEach)
+	// index = numerical index of this element in the array (ditto ^)
+
+	var newdiv = document.createElement("div"); // make an empty div tag
+	newdiv.classList.add("letter"); // add the class "letter" to it
+
+	var image = document.createElement("img"); // make an empty img tag
+	image.src = "images/asl_" + element + ".gif"; // set the source dynamically
+
+	newdiv.id = element; // set the id of this div to match the letter
+
+	newdiv.appendChild(image); // stick the image in the div
+	document.querySelector(".saranwrap").appendChild(newdiv); // add the div to the wrapper on the page
+}
+
+
+
+// ok, now we need some interaction...
+// this is the event listener from last class:
 var textfield = document.querySelector("#enteredText");
 var submitbutton = document.querySelector("#textSubmit");
-
 submitbutton.addEventListener("click", do_stuff);
+
+// how would you add a listener to each of the "letter" divs?
+
+
+
 
 function do_stuff() {
 	var input_string = textfield.value;
+	console.log("text field: " + input_string);
 
-	var the_four_letters = [];
-
-	for (var counter = 0; counter < 4; counter++) {
-		console.log("this is loop #" + counter);
-		the_four_letters.push(input_string.charAt(counter));
-		console.log(the_four_letters);
-		console.log("--- loop over! ---");
-	}
-
-	for (var i = 0; i < 4; i++) {
-		var the_file_i_want = "asl_" + the_four_letters[i] + ".gif";
-
-		console.log(the_file_i_want);
-	}
-
-	// console.log(the_four_letters);
-	console.log("-----------------");
-	console.log(input_string);	
-	console.log(input_string.charAt(0));
-
-	
+	// just so you remember how to get text from a form input
 }
